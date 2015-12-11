7e898fc791c1e16a6f735e22063037eac9c97f23
Mark van de Korput <dr.theman@gmail.com>
2015-12-11 16:21:44 +0100

    json recording player works, but doesn't consider frame timestamps yet

--- python/JSON_player.py
+++ python/JSON_player.py
@@ -2,6 +2,12 @@ import sys
 from natnet_parser import NatNetParser
 import json
 from datetime import datetime
+from OSC_link import OSCLink
+
+# dummy class to represent parsed json rigidbody objects,
+# TODO: figure out which class the natnet parser uses
+class RigidBodyClass:
+	pass
 
 class JSONPlayer:
 	def __init__(self, path=None, natnet=None):
@@ -13,7 +19,7 @@ class JSONPlayer:
 		self.file = None
 
 		if self.natnet == None:
-			natnet = NatNetParser() # we'll just create our own, no need to connect anyway
+			self.natnet = NatNetParser() # we'll just create our own, no need to connect anyway
 
 	def start(self):
 		self.file = open(self.path, 'r')
@@ -32,9 +38,30 @@ class JSONPlayer:
 			self.file.close()
 			self.file = None
 
-	def _playFrame(self, data):
-		print("TODO: play: "+data)
+	def _playFrame(self, data_text):
+		#print("TODO: play: "+data)
+		try:
+			data = json.loads(data_text)
+		except (ValueError):
+			return # invalid line, could be a comment
 
+		self.natnet.rigidbodies = []
+
+		for b in data['rigidbodies']:
+			rb = RigidBodyClass() # TODO: figure out what class the original natnet parses uses/gets
+			rb.id = b['id']
+			rb.position = b['p']
+			rb.orientation = b['r']
+			self.natnet.rigidbodies.append(rb)
+
+		self.natnet.updated()
+
+
+def updateOSC():
+    global player
+    global osc
+    for rb in player.natnet.rigidbodies:
+        osc.sendRigibodyAsJSON(rb)
 
 
 if __name__ == "__main__":
@@ -43,6 +70,9 @@ if __name__ == "__main__":
 		exit(1)
 	
 	filePath = sys.argv[1]		
-
 	player = JSONPlayer(path=filePath)
+
+	osc = OSCLink("145.107.192.137", 8080)
+	player.natnet.updated += updateOSC
+
 	player.start()
