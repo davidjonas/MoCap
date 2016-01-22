54d268e12bd2df695ea88b001364314f35ae0f14
Mark van de Korput <dr.theman@gmail.com>
2016-01-22 17:58:48 +0100

    osc_reader and console_writer for debugging

--- python/mocap_bridge/console_writer.py
+++ python/mocap_bridge/console_writer.py
@@ -5,10 +5,20 @@ from natnet_writer import NatNetWriter
 
 class ConsoleWriter(NatNetWriter):
 
+    def __init__(self, delay=0.5):
+        # params
+        self.delay=delay
+        # attributes
+        self.lastTime = None
+
     def openStream(self): pass
 
     def closeStream(self): pass
 
     def writeDataFrame(self, reader, timestamp):
+        if self.delay != None and self.lastTime != None and reader.getTime() < (self.lastTime + self.delay):
+            return
+
         line = "{0}: {1} rigid bodies and {2} skeletons".format(reader.getTime(), len(reader.rigidbodies), len(reader.skeletons))
         print(line)
+        self.lastTime = reader.getTime()
