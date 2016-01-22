e8895ee59eca3ab705e74bc8b2aa348d3dc24920
Mark van de Korput <dr.theman@gmail.com>
2016-01-22 14:54:28 +0100

    make python 'mocap_bridge' package

--- python/mocap_bridge/play_recording.py
+++ python/mocap_bridge/play_recording.py
@@ -1,7 +1,9 @@
 from natnet_pipeline import NatNetPipeline
 from default_readers import JSONNatNetReader
 from default_writers import OSCNatNetWriter
+
 import sys
+
 
 #pipeline = NatNetPipeline(JSONNatNetReader(sys.argv[1]), OSCNatNetWriter())
 pipeline = NatNetPipeline(JSONNatNetReader("data/sampleRecording3.json"), OSCNatNetWriter())
