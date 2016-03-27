89e4d25a0bfc98168c23b252a87e9ef79d247d44
Mark van de Korput <dr.theman@gmail.com>
2016-03-27 15:18:37 +0200

    added auto-config python logic to MoCapJSON blender addon

--- blender/addons/MoCapJSON.py
+++ blender/addons/MoCapJSON.py
@@ -71,10 +71,112 @@ class Panel(bpy.types.Panel):
         config = context.object.moCapJsonConfig
 
         if config.enabled == True:
-          layout.row().prop(config, "file")
-          layout.row().prop(config, "loop")
-          layout.row().prop(config, "sync")
+            layout.row().prop(config, "file")
+            layout.row().prop(config, "loop")
+            layout.row().prop(config, "sync")
 
+            mcjObj = MoCapJsonObj(context.object)
+            # game logic connection not complete; inform user and provide
+            # buttons to auto-configure
+            if mcjObj.gameLogicConnection() == True:
+              layout.row().label(text='Game logic: OK')
+            else:
+              msgs = []
+
+              if mcjObj.sensor() == None:
+                msgs.append('Sensor: NO')
+              else:
+                msgs.append('Sensor: OK')
+
+              if mcjObj.controller() == None:
+                msgs.append('Controller: NO')
+              else:
+                msgs.append('Controller: OK')
+
+              msgs.append('Connection: NO')
+              layout.row().label(text=', '.join(msgs))
+              layout.row().operator("object.mocap_json_config_game_logic", text="Configure")
+
+
+# This class provides MoCapJson-related information (read-only)
+# about a blender object (not a game engine object)
+class MoCapJsonObj:
+    """docstring for """
+    def __init__(self, object):
+        self.object = object
+
+    def gameLogicConnection(self):
+        # if there is no valid sensor, then there can be no connection either
+        if self.sensor() == None:
+            return False
+
+        # see if the sensor is hooked up to our controller
+        # that is assuming there IS a valid controller
+        for controller in self.sensor().controllers:
+            if controller == self.controller():
+                return True
+
+        # no dice
+        return False
+
+    def sensor(self):
+        # loop through all of this object's game logic sensors
+        for sensor in self.object.game.sensors:
+            # find one that is appropriately configured
+            if sensor.type == 'ALWAYS' and sensor.use_pulse_false_level == False and sensor.use_pulse_true_level == True:
+                # return it if found
+                return sensor
+        # doesn't exist (yet)
+        return None
+
+    # returns a controller reference to a appropriaely configured python module controller
+    def controller(self):
+        for controller in self.object.game.controllers:
+            if controller.type == 'PYTHON' and controller.mode == 'MODULE' and controller.module == 'MoCapJSON.update':
+                return controller
+        return None
+
+class MoCapJsonConfigGameLogicOperator(bpy.types.Operator):
+    bl_idname = "object.mocap_json_config_game_logic"
+    bl_label = "Creates the game logic sensor and controller that requires MoCapJSON to work"
+    bl_description = "Creates an Always sensor and a Python module controller and links them"
+
+    # @classmethod
+    # def poll(cls, context):
+    #     return True
+
+    def execute(self, context):
+      obj = MoCapJsonObj(context.object)
+      if obj.controller() == None:
+        self.createController(context)
+
+      if obj.sensor() == None:
+        self.createSensor(context)
+
+      if obj.gameLogicConnection() != True:
+        # create the connection
+        print("MoCapJSON creating game logic link")
+        obj.sensor().link(obj.controller())
+
+      return {'FINISHED'}
+
+    def createController(self, context):
+        print("MoCapJSON creating Python module controller")
+        # create empty PYTHON controller
+        bpy.ops.logic.controller_add(type='PYTHON')
+        # get reference to newly created controller
+        c = context.object.game.controllers[-1]
+        # configure appropriately
+        c.mode = 'MODULE'
+        c.module = 'MoCapJSON.update'
+
+    def createSensor(self, context):
+        print('MoCapJSON creating Always sensor')
+        # create the sensor
+        bpy.ops.logic.sensor_add(type='ALWAYS')
+        # get reference to newly created sensor
+        s = context.object.game.sensors[-1]
+        s.use_pulse_true_level = True
 
 # This class represents the config data (that the UI Panel interacts with)
 class Config(bpy.types.PropertyGroup):
